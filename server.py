442f59ee065d419b704de01973aa0cb960d74035
jjordan-quantum <40579554+jjordan-quantum@users.noreply.github.com>
2021-05-01 13:33:36 -0700

    Update server.py to test app display text

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'Hello world! \nWelcome to yieldfarmer001! You requested the following route %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
