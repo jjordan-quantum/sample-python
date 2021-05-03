2b650b74f22ed11058f54d521922350cbd0f4b2e
jjordan-quantum <constant.space@gmail.com>
2021-05-02 22:35:03 -0700

    update

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         msg = 'Welcome to yieldfarmer001! You requested the following route %s' % (self.path)
-        if self.path == '/new':
+        if self.path == '/runtest':
             msg = 'This is a new path: %s ... runing run_test function' % (self.path)
             liquidity_example.run_test()
         self.wfile.write(msg.encode())
