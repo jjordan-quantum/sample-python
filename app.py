115403a119545c80f768857f45d2bd1c5e86d990
jjordan-quantum <constant.space@gmail.com>
2021-05-03 19:41:03 -0700

    new commit

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from lib import functions
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Sammy!'
+    return 'Hello World!'
 
-@app.route('/runtest')
+@app.route('/run_test')
 def hello_runtest():
-    return 'Hello! This is the runtest route'
+    msg = 'Hello! This is the run_test route'
+    return msg
+
+@app.route('/safe_add')
+def hello_safeadd():
+    sum = functions.safe_add(1+2)
+    msg = f'Hello! This is the safe_add route\n, 1 + 2 = {sum}'
+    return msg
---
+++ lib/functions.py
@@ -0,0 +1,2 @@
+def safe_add(num_1, num_2):
+    return 1 + 2
