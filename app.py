5c249d3e358191842d17c63de7f5fb0bf70111e9
jjordan-quantum <constant.space@gmail.com>
2021-05-03 19:31:25 -0700

    new commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello Sammy!'
+
+@app.route('/runtest')
+def hello_runtest():
+    return 'Hello! This is the runtest route'
